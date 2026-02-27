8022c3a81baa585ea69e69bc9e806c800d1540a6
duoz0323 <hduongnguyen04@gmail.com>
2026-02-27 18:20:45 +0700

    off db

--- Backend_Nhom10/src/main/java/stu/edu/Backend_Nhom10/BackendNhom10Application.java
+++ Backend_Nhom10/src/main/java/stu/edu/Backend_Nhom10/BackendNhom10Application.java
@@ -2,8 +2,9 @@ package stu.edu.Backend_Nhom10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BackendNhom10Application {
 
 	public static void main(String[] args) {
