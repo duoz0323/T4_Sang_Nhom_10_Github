4ca1a111cb0369c0b185281e53c3d075f02badd1
duoz0323 <hduongnguyen04@gmail.com>
2026-02-27 18:06:40 +0700

    first update

---
+++ Backend_Nhom10/src/main/java/stu/edu/Backend_Nhom10/BackendNhom10Application.java
@@ -0,0 +1,13 @@
+package stu.edu.Backend_Nhom10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNhom10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNhom10Application.class, args);
+	}
+
+}
---
+++ Backend_Nhom10/src/test/java/stu/edu/Backend_Nhom10/BackendNhom10ApplicationTests.java
@@ -0,0 +1,13 @@
+package stu.edu.Backend_Nhom10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNhom10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
