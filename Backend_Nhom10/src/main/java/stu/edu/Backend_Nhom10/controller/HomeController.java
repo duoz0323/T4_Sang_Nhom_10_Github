ab27e2e656cefe9465a4f412a22b9e6ca96a4791
duoz0323 <hduongnguyen04@gmail.com>
2026-03-14 13:52:13 +0700

    home controller up

--- Backend_Nhom10/src/main/java/stu/edu/Backend_Nhom10/controller/HomeController.java
+++ Backend_Nhom10/src/main/java/stu/edu/Backend_Nhom10/controller/HomeController.java
@@ -1,13 +1,13 @@
 package stu.edu.Backend_Nhom10.controller; 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Backend Java Nhóm 10 đã sẵn sàng và đang chạy 24/7!";
+        return "redirect:/swagger-ui/index.html";
     }
 }
