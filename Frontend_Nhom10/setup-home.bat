b83b519c32df465b32a0d199d1f02291ebc0b113
MinhKhanh <minhkhanh200424121@gmail.com>
2026-03-30 09:47:05 +0700

    remove unused scripts and cleanup directory structure

--- Frontend_Nhom10/setup-home.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d d:\Nhom10\T4_Sang_Nhom_10_Github\Frontend_Nhom10
-mkdir src\features\home\pages 2>nul
-mkdir src\features\home\components 2>nul
-echo Directories created successfully
-node create-home-dirs.js
