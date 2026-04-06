c3816a04bcb03d01a3775246496a3f1b476bbfa5
MinhKhanh <minhkhanh200424121@gmail.com>
2026-04-06 13:15:15 +0700

    merge

--- Backend_Nhom10/src/main/java/stu/edu/Backend_Nhom10/configuration/SecurityConfig.java
+++ Backend_Nhom10/src/main/java/stu/edu/Backend_Nhom10/configuration/SecurityConfig.java
@@ -24,6 +24,12 @@ public class SecurityConfig {
             "/applications/public"
     };
 
+    private final String[] PUBLIC_GET_ENDPOINTS = {
+            "/posts/public",
+            "/posts/**",
+            "/locations/search"
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request -> request
@@ -33,6 +39,7 @@ public class SecurityConfig {
                         "/swagger-ui.html"
                 ).permitAll()
                 .requestMatchers(HttpMethod.POST, PUBLIC_POST_ENDPOINTS).permitAll()
+                .requestMatchers(HttpMethod.GET, PUBLIC_GET_ENDPOINTS).permitAll()
                 .requestMatchers("/").permitAll()
                 .anyRequest().authenticated());
 
